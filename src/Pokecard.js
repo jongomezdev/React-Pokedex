c6e5f7eec73aedf78fb61390797064ea3e48f144
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-27 22:35:34 -0500

    added some styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -9,9 +9,9 @@ class Pokecard extends Component {
     return (
       <div className="Pokecard">
         <h1>{this.props.name}</h1>
-        <img src={imgSrc} alt={this.props.name} />
-        <div>Type: {this.props.type}</div>
-        <div>EXP: {this.props.exp}</div>
+        <img className="Pokemon" src={imgSrc} alt={this.props.name} />
+        <div className="Pokecard-data">Type: {this.props.type}</div>
+        <div className="Pokecard-data">EXP: {this.props.exp}</div>
       </div>
     );
   }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Pokecard from "./Pokecard";
+import "./Pokedex.css";
 
 class Pokedex extends Component {
   static defaultProps = {
@@ -18,14 +19,16 @@ class Pokedex extends Component {
     return (
       <div className="Pokedex">
         <h1>Pokedex!</h1>
-        {this.props.pokemon.map((p) => (
-          <Pokecard
-            id={p.id}
-            name={p.name}
-            type={p.type}
-            exp={p.base_experience}
-          />
-        ))}
+        <div className="Pokedex-cards">
+          {this.props.pokemon.map((p) => (
+            <Pokecard
+              id={p.id}
+              name={p.name}
+              type={p.type}
+              exp={p.base_experience}
+            />
+          ))}
+        </div>
       </div>
     );
   }
