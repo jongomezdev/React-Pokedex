c6a4aaefc9b9a9baf9f6de757100ddcdcd064a7e
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-27 21:51:06 -0500

    render the array of pokemon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import Pokecard from "./Pokecard";
+import Pokedex from "./Pokedex";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokecard id={4} name="Charmander" type="Fire" exp={62} />
+        <Pokedex />
       </div>
     );
   }
