e0674fca0ee52eaa98412c6c02d6aaab7f3efd86
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-28 16:40:08 -0500

    successfully render api results

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -3,19 +3,39 @@ import "./Pokecard.css";
 // const POKE_API =
 //   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+// const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
 
-let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
+// let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 class Pokecard extends Component {
+  state = {
+    name: "",
+    imageUrl: "",
+    pokemonIndex: "",
+  };
+
+  componentDidMount() {
+    const { name } = this.props;
+    const pokemonIndex = (number) =>
+      number <= 999 ? `00${number}`.slice(-3) : number;
+    const imageUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokemonIndex}.png`;
+
+    this.setState({
+      name: name,
+      imageUrl: imageUrl,
+      pokemonIndex: pokemonIndex,
+    });
+  }
   render() {
-    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
+    // let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
     return (
       <div className="Pokecard">
-        <h1>{this.props.name}</h1>
-        <img className="Pokemon" src={imgSrc} alt={this.props.name} />
+        <h1>{this.state.name}</h1>
+        <img src={this.state.imageUrl} alt={this.state.name} />
+        {/* <img className="Pokemon" src={imageUrl} alt={this.state.name} /> */}
+        {/* <img className="Pokemon" src={imgSrc} alt={this.props.name} />
         <div className="Pokecard-data">Type: {this.props.type}</div>
-        <div className="Pokecard-data">EXP: {this.props.exp}</div>
+        <div className="Pokecard-data">EXP: {this.props.exp}</div> */}
       </div>
     );
   }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,37 +1,65 @@
 import React, { Component } from "react";
 import Pokecard from "./Pokecard";
 import "./Pokedex.css";
+import axios from "axios";
 
 class Pokedex extends Component {
-  static defaultProps = {
-    pokemon: [
-      { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-      { id: 6, name: "Charizard", type: "fire", base_experience: 200 },
-      { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-      { id: 8, name: "Wartortle", type: "water", base_experience: 120 },
-      { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-      { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-      { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-      { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-      { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-      { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
-    ],
+  // static defaultProps = {
+  //   pokemon: [
+  //     { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+  //     { id: 6, name: "Charizard", type: "fire", base_experience: 200 },
+  //     { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+  //     { id: 8, name: "Wartortle", type: "water", base_experience: 120 },
+  //     { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+  //     { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+  //     { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+  //     { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+  //     { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+  //     { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
+  //   ],
+  // };
+
+  state = {
+    url: "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0",
+    pokemon: null,
   };
+
+  async componentDidMount() {
+    const res = await axios.get(this.state.url);
+    this.setState({ pokemon: res.data["results"] });
+  }
+
   render() {
     return (
       <div className="Pokedex">
-        <h1>Pokedex!</h1>
-        <div className="Pokedex-cards">
-          {this.props.pokemon.map((p) => (
-            <Pokecard
-              id={p.id}
-              name={p.name}
-              type={p.type}
-              exp={p.base_experience}
-            />
-          ))}
-        </div>
+        {this.state.pokemon ? (
+          <div className="Pokedex-cards">
+            {this.state.pokemon.map((pokemon) => (
+              <Pokecard
+                key={pokemon.name}
+                name={pokemon.name}
+                url={pokemon.url}
+              />
+            ))}
+          </div>
+        ) : (
+          <h1>Loading Pokemon</h1>
+        )}
       </div>
+
+      // <div className="Pokedex">
+      //   <h1>Pokedex!</h1>
+      //   <div className="Pokedex-cards">
+      //     {this.props.pokemon.map((p) => (
+      //       <Pokecard
+      //         id={p.id}
+      //         name={p.name}
+      //         type={p.type}
+      //         exp={p.base_experience}
+      //       />
+      //     ))}
+      //   </div>
+      // </div>
     );
   }
 }
