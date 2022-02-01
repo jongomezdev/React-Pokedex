12121ac8c418d38d62db24574e1122e00ff86d3b
Warrior of Zarona <andrewbautista85@gmail.com>
2022-01-31 21:12:20 -0500

    App.js refactored to functional component, fixed tests and updated package.json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Pokedex from "./Pokedex";
+import {Pokedex} from "./Pokedex";
 import "./App.css";
 import Pokemon from "./Pokemon";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route exact path="/" component={Pokedex} />
-            <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
-            <Pokedex />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Pokedex} />
+          <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+          <Pokedex />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import React from 'react'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders App.js', () => {
+    render(<App />);
+
+    expect(screen.getByText('Pokedex!')).toBeVisible();
+
+  })
 });
